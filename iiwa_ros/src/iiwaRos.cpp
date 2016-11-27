6787cc95ec644edb368fc991384ab290ea9da15e
Sebastian Requena <Sebastian Requena>
2016-11-27 15:38:38 +0100

    This commit should fix the compiler error for Issue #17

--- iiwa_ros/src/iiwaRos.cpp
+++ iiwa_ros/src/iiwaRos.cpp
@@ -1,31 +1,32 @@
-/**  
+/**
  * Copyright (C) 2016 Salvatore Virga - salvo.virga@tum.de, Marco Esposito - marco.esposito@tum.de
  * Technische Universität München
  * Chair for Computer Aided Medical Procedures and Augmented Reality
  * Fakultät für Informatik / I16, Boltzmannstraße 3, 85748 Garching bei München, Germany
  * http://campar.in.tum.de
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
  * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
- * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * \author Salvatore Virga
  * \version 2.0.0
  * \date 07/03/2016
  */
 
 #include "iiwaRos.h"
+#include<string>
 
 using namespace std;
 
@@ -33,26 +34,27 @@ ros::Time last_update_time;
 
 iiwaRos::iiwaRos() { }
 
-void iiwaRos::init(bool initRos) 
+void iiwaRos::init(bool initRos)
 {
     // hide ROS away for non-ROS applications
     if (initRos) {
         int argc = 1;
         char *argv[1];
-        argv[0] = "iiwaRos";
+        string str = "iiwaRos";
+        argv[0] = str.c_str();  //ToDo check that argv shouln't be a const
         ros::init(argc, argv, "iiwaRos");
     }
-    
+
     ros::NodeHandle node_handle;
-        
+
     robot_is_connected_ = false;
-    
+
     holder_state_pose.init("state/CartesianPose");
     holder_state_joint_position.init("state/JointPosition");
     holder_state_joint_torque.init("state/JointTorque");
 //     holder_state_wrench.init("state/CartesianWrench");
 //     holder_state_joint_stiffness.init("state/JointStiffness");
-    
+
     holder_command_pose.init("command/CartesianPose");
     holder_command_joint_position.init("command/JointPosition");
 //     holder_command_wrench.init("command/CartesianWrench");
@@ -71,7 +73,7 @@ iiwaRos::~iiwaRos() { }
 //         if (is_connected != robot_is_connected_)
 //             if (is_connected)
 //                 ROS_INFO("IIWA robot is connected.");
-//             else 
+//             else
 //                 ROS_WARN("IIWA robot is NOT connected.");
 //         robot_is_connected_ = is_connected;
 //     }
@@ -124,9 +126,9 @@ void iiwaRos::setCommandJointPosition(const iiwa_msgs::JointPosition& position) 
 // void iiwaRos::setCommandCartesianWrench(const geometry_msgs::WrenchStamped& wrench) {
 //     command_cartesian_wrench_ = wrench;
 // }
-// 
+//
 // void iiwaRos::setCommandJointStiffness(const iiwa_msgs::JointStiffness& stiffness)  {
-//     command_joint_stiffness_ = stiffness;  
+//     command_joint_stiffness_ = stiffness;
 // }
 // void iiwaRos::setCommandJointTorque(const iiwa_msgs::JointTorque& torque)  {
 //     command_joint_torque_ = torque;
