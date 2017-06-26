adc900772c249192645d0e32ae24e2c50bd62483
Salvo Virga <salvo.virga@tum.de>
2017-06-26 15:49:19 +0200

    well formatted
    location of the headers actually changed now, this will break some code

--- iiwa_ros/src/time_to_destination_service.cpp
+++ iiwa_ros/src/time_to_destination_service.cpp
@@ -6,53 +6,72 @@
  * http://campar.in.tum.de
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
  *
- * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
- * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY,
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <time_to_destination_service.h>
+#include <iiwa_ros/time_to_destination_service.h>
 
-namespace iiwa_ros {
-  
-  TimeToDestinationService::TimeToDestinationService() : iiwaServices< iiwa_msgs::TimeToDestination >() {}
-  
-  TimeToDestinationService::TimeToDestinationService(const std::string& service_name, const bool verbose) : iiwaServices< iiwa_msgs::TimeToDestination >(service_name, verbose) {}
-  
-  double TimeToDestinationService::getTimeToDestination() {
-    if (service_ready_) {
-      
-      if (callService()) {
-        return time_to_destination_;
-      }
-      else {
-        return -999; // It cannot return -1 since it might be a meaningfull result.
-      }
+namespace iiwa_ros
+{
+TimeToDestinationService::TimeToDestinationService() : iiwaServices<iiwa_msgs::TimeToDestination>()
+{
+}
+
+TimeToDestinationService::TimeToDestinationService(const std::string& service_name, const bool verbose)
+  : iiwaServices<iiwa_msgs::TimeToDestination>(service_name, verbose)
+{
+}
+
+double TimeToDestinationService::getTimeToDestination()
+{
+  if (service_ready_)
+  {
+    if (callService())
+    {
+      return time_to_destination_;
     }
-    ROS_ERROR_STREAM("The service client was not intialized yet.");
+    else
+    {
+      return -999;  // It cannot return -1 since it might be a meaningfull result.
+    }
   }
-  
-  bool TimeToDestinationService::callService() {
-    if (client_.call(config_)) {
-      if(verbose_) {
-        ROS_INFO_STREAM(ros::this_node::getName() << ": " << service_name_ << " successfully called.");
-      }
-      time_to_destination_ = config_.response.remaining_time;
-      return true;
+  ROS_ERROR_STREAM("The service client was not intialized yet.");
+}
+
+bool TimeToDestinationService::callService()
+{
+  if (client_.call(config_))
+  {
+    if (verbose_)
+    {
+      ROS_INFO_STREAM(ros::this_node::getName() << ": " << service_name_ << " successfully called.");
     }
-    else if (verbose_) {
-      ROS_ERROR_STREAM(service_name_ << " could not be called");
-    }
-    return false;		
+    time_to_destination_ = config_.response.remaining_time;
+    return true;
   }
+  else if (verbose_)
+  {
+    ROS_ERROR_STREAM(service_name_ << " could not be called");
+  }
+  return false;
+}
 }
