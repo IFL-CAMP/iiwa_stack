3be75e9fc63c717e777c52bdde8fbc6115005448
Salvo Virga <salvo.virga@tum.de>
2019-02-06 04:50:36 -0800

    moved random functions to an utility class

--- iiwa_ros_java/src/de/tum/in/camp/kuka/ros/app/ROSSmartServo.java
+++ iiwa_ros_java/src/de/tum/in/camp/kuka/ros/app/ROSSmartServo.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (C) 2016-2019 Salvatore Virga - salvo.virga@tum.de, Marco Esposito - marco.esposito@tum.de
- * Technische Universität München Chair for Computer Aided Medical Procedures and Augmented Reality Fakultät
- * für Informatik / I16, Boltzmannstraße 3, 85748 Garching bei München, Germany http://campar.in.tum.de All
+ * Technische Universitï¿½t Mï¿½nchen Chair for Computer Aided Medical Procedures and Augmented Reality Fakultï¿½t
+ * fï¿½r Informatik / I16, Boltzmannstraï¿½e 3, 85748 Garching bei Mï¿½nchen, Germany http://campar.in.tum.de All
  * rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided
@@ -300,7 +300,7 @@ public class ROSSmartServo extends ROSBaseApplication {
         });
 
     // Execute the subscriber node.
-    nodeMainExecutor.execute(subscriber, nodeConfSubscriber);
+    nodeMainExecutor.execute(subscriber, subscriberNodeConfiguration);
   }
 
   // TODO move this somewhere else.
